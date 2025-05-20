9a71c5c5d3048b983437af1315ced6db5c7f9d5f
Ashokan <vignesh.ashokan@accenture.com>
2025-05-20 23:20:21 +0530

    eighth commit

--- src/blocks/TextAnimations/TextPressure/TextPressure.tsx
+++ src/blocks/TextAnimations/TextPressure/TextPressure.tsx
@@ -220,7 +220,10 @@ const TextPressure: React.FC<TextPressureProps> = ({
         {chars.map((char, i) => (
           <span
             key={i}
-            ref={el => (spansRef.current[i] = el)}
+            ref={el => {
+              spansRef.current[i] = el;
+              return undefined;
+            }}
             data-char={char}
             style={{
               display: 'inline-block',
