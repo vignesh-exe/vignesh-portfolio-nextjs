a3871c95a6dfc1afad8317200b5b115f44d609d4
Ashokan <vignesh.ashokan@accenture.com>
2025-05-26 17:51:42 +0530

    23th commit

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -18,7 +18,27 @@ export async function generateMetadata() {
 export default function Blog() {
   return (
     <Column maxWidth="l" style={{ marginTop: '40px' }}>
+      <Schema
+        as="blog"
+        baseURL={baseURL}
+        title={blog.title}
+        description={blog.description}
+        path={blog.path}
+        image={`${baseURL}/og?title=${encodeURIComponent(blog.title)}`}
+        author={{
+          name: person.name,
+          url: `${baseURL}/blog`,
+          image: `${baseURL}${person.avatar}`
+        }}
+      />
+
       {newsletter.display && <Mailchimp newsletter={newsletter} />}
+      <Heading marginBottom="l" variant="display-strong-s">
+        {blog.title}
+      </Heading>
+      <Column fillWidth flex={1}>
+        <Posts range={[1, 2]} columns="2" />
+      </Column>
     </Column>
   );
 }
